c0d0b8aaf5966dce4eea282de4041a439d45dc80
spanishigirl <users.noreply.github.com>
2025-05-08 20:41:39 +0200

    composer update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'lampace/base',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '77dd3121d50e7531c47ecf75a8a3bfdf0d51d618',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'lampace/base' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '77dd3121d50e7531c47ecf75a8a3bfdf0d51d618',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
